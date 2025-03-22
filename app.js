744bdd3cd2eb259fc74a5120ce68cdf7b32dc638
Charaka Gunasinghe <charaka.info@gmail.com>
2025-03-22 14:19:30 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello world...');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+// initialize the express app
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// TODO: setup routing paths
+
+// TODO: error handler middleware
+
+// start the server
+const ENV = process.env.ENV || 'development';
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`${ENV} | ${PORT}`);
+});
